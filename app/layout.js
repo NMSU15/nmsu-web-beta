cba22471b4c83f55f2758edad62b7706694579c1
NMSU15 <student_union@nmit.edu.mn>
2024-10-11 22:25:39 +0800

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,61 @@
+// import localFont from "next/font/local";
+import { Rubik } from 'next/font/google'
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata = {
+  title: {
+    template: '%s | NMSU',
+    default: 'New Mongol Student Union',
+  },
+  description: "NEW MONGOL STUDENT UNION",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/favicon.ico',
+        href: '/favicon.ico',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/favicon-dark.ico',
+        href: '/favicon-dark.ico',
+      },
+    ],
+  },
+  openGraph: {
+    title: 'New Mongol Student Union',
+    metadataBase: new URL("https://student.nmit.edu.mn/"),
+    description: 'New Mongol Student Union',
+    url: 'https://student.nmit.edu.mn/',
+    siteName: 'New Mongol Student Union',
+    images: [
+      {
+        url: 'https://student.nmit.edu.mn/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'background',
+      },
+    ],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="mn">
+      <body className={rubik.className}>
+        <Providers>
+        <Navbar/>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
